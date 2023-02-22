fcbc38490eaac8e112acb0ec59873b4517224d78
Blackjac12 <124262044+Blackjac12@users.noreply.github.com>
2023-02-22 10:46:44 -0500

    Add files via upload

---
+++ num=1.py
@@ -0,0 +1,9 @@
+num=1
+while num!=0:
+    print("digigete un numero: ")
+    num=int(input())
+    if num%2==0:
+        print("el numero es par")
+    else:
+        print("el numero es impar")
+print("finalizo el programa")        
