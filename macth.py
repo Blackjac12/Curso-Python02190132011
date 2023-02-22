cef369e19d5f822ae75364c7cc196f2719594441
Blackjac12 <124262044+Blackjac12@users.noreply.github.com>
2023-02-22 10:47:00 -0500

    Add files via upload

---
+++ macth.py
@@ -0,0 +1,28 @@
+print("digite un numero del 1 al 12")
+num=int(input())
+
+match num:
+    case 1:
+        print("enero ")
+    case 2:
+        print("febrero")
+    case 3:
+        print ("marzo")
+    case 4:
+        print ("abril")
+    case 5:
+        print("mayo") 
+    case 6:
+        print("junio")
+    case 7:
+        print ("julio")                           
+    case 8:    
+        print ("agosto")
+    case 9:
+        print("septiembre")
+    case 10:
+        print("octubre")
+    case 11:
+        print("nombrieve") 
+    case 12:
+        print("diciembre")
