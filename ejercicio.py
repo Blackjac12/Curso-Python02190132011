efccfb518bd3fd4daaa0664c99e1b4c5e2445412
Blackjac12 <124262044+Blackjac12@users.noreply.github.com>
2023-02-22 10:46:29 -0500

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,9 @@
+print("digite un numero: ")
+numero =int(input())
+
+if numero >0 :
+    print("El numero es positivo ")
+elif numero ==0:
+    print("el numero es Neutro")
+else:
+    print("El numero es negtivo")    
