1537afaa7c338e2d9cd12fc3f778de586b12770f
Blackjac12 <124262044+Blackjac12@users.noreply.github.com>
2023-02-27 19:59:12 -0500

    Update coductores.py

--- taller/coductores.py
+++ taller/coductores.py
@@ -1,24 +1,29 @@
-conductor_km = {}
+# Creamos variables para almacenar los nombres de los conductores y sus kilómetros conducidos
+conductor1_km_lunes = int(input("Kilómetros conducidos por conductor 1 el lunes: "))
+conductor1_km_martes = int(input("Kilómetros conducidos por conductor 1 el martes: "))
+conductor1_km_miercoles = int(input("Kilómetros conducidos por conductor 1 el miércoles: "))
+conductor1_km_jueves = int(input("Kilómetros conducidos por conductor 1 el jueves: "))
+conductor1_km_viernes = int(input("Kilómetros conducidos por conductor 1 el viernes: "))
 
-while True:
-    # Pedir el nombre del conductor y los kilómetros conducidos en una semana
-    conductor = input("Ingrese el nombre del conductor: ")
-    km_semana = float(input("Ingrese los kilómetros conducidos esta semana por el conductor: "))
+conductor2_km_lunes = int(input("Kilómetros conducidos por conductor 2 el lunes: "))
+conductor2_km_martes = int(input("Kilómetros conducidos por conductor 2 el martes: "))
+conductor2_km_miercoles = int(input("Kilómetros conducidos por conductor 2 el miércoles: "))
+conductor2_km_jueves = int(input("Kilómetros conducidos por conductor 2 el jueves: "))
+conductor2_km_viernes = int(input("Kilómetros conducidos por conductor 2 el viernes: "))
 
-    # Verificar si el conductor ya está en el diccionario
-    if conductor in conductor_km:
-        # Si el conductor ya está en el diccionario, sumar los kilómetros
-        conductor_km[conductor] += km_semana
-    else:
-        # Si el conductor no está en el diccionario, agregarlo con los kilómetros de la semana
-        conductor_km[conductor] = km_semana
+conductor3_km_lunes = int(input("Kilómetros conducidos por conductor 3 el lunes: "))
+conductor3_km_martes = int(input("Kilómetros conducidos por conductor 3 el martes: "))
+conductor3_km_miercoles = int(input("Kilómetros conducidos por conductor 3 el miércoles: "))
+conductor3_km_jueves = int(input("Kilómetros conducidos por conductor 3 el jueves: "))
+conductor3_km_viernes = int(input("Kilómetros conducidos por conductor 3 el viernes: "))
 
-    # Preguntar si se desea agregar más conductores
-    seguir = input("¿Desea ingresar más conductores? (s/n): ")
-    if seguir.lower() == "n":
-        break
+conductor4_km_lunes = int(input("Kilómetros conducidos por conductor 4 el lunes: "))
+conductor4_km_martes = int(input("Kilómetros conducidos por conductor 4 el martes: "))
+conductor4_km_miercoles = int(input("Kilómetros conducidos por conductor 4 el miércoles: "))
+conductor4_km_jueves = int(input("Kilómetros conducidos por conductor 4 el jueves: "))
+conductor4_km_viernes = int(input("Kilómetros conducidos por conductor 4 el viernes: "))
 
-# Imprimir los resultados
-print("Kilómetros conducidos por conductor:")
-for conductor, km in conductor_km.items():
-    print(f"{conductor}: {km} km")
+# Podemos realizar operaciones con estas variables para calcular las cantidades que necesitemos
+total_km_conductor1 = conductor1_km_lunes + conductor1_km_martes + conductor1_km_miercoles + conductor1_km_jueves + conductor1_km_viernes
+
+print("El conductor 1 condujo un total de", total_km_conductor1, "kilómetros durante la semana.")
